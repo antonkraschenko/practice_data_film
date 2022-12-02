e478c494da340f8b78409693aae7e4cc5dcea392
Anton <antonkraschenko@ukr.net>
2022-12-02 19:45:59 +0200

    decided the first task

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const numberOfFilms = +prompt('How many films did you see?','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres: {},
+    privat: false
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?','');
+const b = +prompt('На сколько оцените его?','');
+const c = prompt('Один из последних просмотренных фильмов?','');
+const d = +prompt('На сколько оцените его?','');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
+
+
+
+
+
