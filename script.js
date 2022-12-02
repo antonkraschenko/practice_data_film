d2d2392b17e47c96ef85a2245cfe53bd4024122b
Anton <antonkraschenko@ukr.net>
2022-12-02 19:53:13 +0200

    decided task 2

--- script.js
+++ script.js
@@ -10,20 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-
-const a = prompt('Один из последних просмотренных фильмов?','');
-const b = +prompt('На сколько оцените его?','');
-const c = prompt('Один из последних просмотренных фильмов?','');
-const d = +prompt('На сколько оцените его?','');
-
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-
 console.log(personalMovieDB);
 
+for (let i = 1; i <= 2; i++) {
+    
+    const a = prompt('Один из последних просмотренных фильмов?','');
+    const b = prompt('На сколько оцените его?','');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
 
+if ( personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель') ;
+} else if (personalMovieDB.count > 30) {
+    alert('Вы классический зритель');
+} else {
+    alert('Произошла ошибка');
+}
 
 
